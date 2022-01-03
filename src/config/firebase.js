b0e0be695a19ef23fdf003973a22c1911edb0524
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2022-01-03 07:19:35 +0000

    added firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBx0GIlcE2HRSulTY9lp14eViZltO312aE",
+  authDomain: "cookbook-53824.firebaseapp.com",
+  projectId: "cookbook-53824",
+  storageBucket: "cookbook-53824.appspot.com",
+  messagingSenderId: "363711045601",
+  appId: "1:363711045601:web:6d71267f4772f42a387ca0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
