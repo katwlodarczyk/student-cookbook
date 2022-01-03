517b42be3337c22ea498dec72ad548447bb37ca4
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2022-01-03 17:53:36 +0000

    still trying to figure it out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect } from "react";
-import { Routes, Route, Link, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate, useLocation } from "react-router-dom";
 import Home from './views/Home';
 import ShoppingList from './views/ShoppingList';
 import WeeklyPlanner from './views/WeeklyPlanner';
@@ -12,17 +12,6 @@ import { getShoppingList, getRecipeCollection } from "./data.js";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "./config/firebase";
 import useAuth from "./services/useAuth";
-
-function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
-}
 
 function Protected({ authenticated, children, ...rest }) {
   return (
@@ -49,10 +38,6 @@ function RequireAuth({ children }) {
   let location = useLocation();
 
   if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
@@ -61,7 +46,7 @@ function RequireAuth({ children }) {
 
 function App() {
   initializeApp(firebaseConfig);
-  const { isAuthenticated, createEmailUser, signInEmailUser, signUserOut } = useAuth();
+  const { isAuthenticated, createEmailUser, signInEmailUser } = useAuth();
   useEffect(() => {
     if (isAuthenticated) {
       // history.push(history.location.state.from.pathname);
@@ -77,18 +62,18 @@ function App() {
   return (
     <div className="App font-koho">
         <Routes>
-          <Route path='/login' element={<Login/>} />
-          <Route path='/register' element={<Register/>} />
+          <Route path='/login' element={<Login signInEmailUser={signInEmailUser} />} />
+          <Route path='/register' element={<Register createEmailUser={createEmailUser}/>} />
           <Route authenticated={isAuthenticated} exact path="/" element={<RequireAuth><Home recipeCollection={recipeCollection}/></RequireAuth>} />
           <Route authenticated={isAuthenticated} exact path="/shopping-list" element={<ShoppingList shoppingList={shoppingList}/>} />
           <Route authenticated={isAuthenticated} exact path="/weekly-planner" element={<WeeklyPlanner />} />
           <Route authenticated={isAuthenticated} exact path={`/recipe/:recipeId`} element={<Recipe />} />
           <Route authenticated={isAuthenticated} exact path={`/recipe/:recipeId/step/:stepId`} element={<CookRecipe />} />
-          <Route path="*" element={<NoMatch />}/>
+          <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
     </div>
   );
 }
 
 
-export default App
+export default App;
--- src/views/Login.js
+++ src/views/Login.js
@@ -32,7 +32,7 @@ const Login = (props) => {
   return (
     <div className="w-full min-h-screen h-max px-4 pt-16 pb-10 flex flex-col space-y-8">
         <div className="flex flex-col justify-center align-center text-center">
-          <img src={logo} alt="logo"/>
+          <img src={logo} alt="logo" className="w-1/3 self-center flex-shrink-0 mb-4"/>
           <h1 className="text-4xl font-galada mt-4 pb-2">Student Cookbook</h1>
           <p className="text-sm px-10">A vast collection of fast and easy recipes with a step by step guide</p>
         </div>
