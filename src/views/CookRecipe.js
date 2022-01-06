7daa9d531cb57710904f95a3152c8915043e908a
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2022-01-06 13:17:49 +0000

    Going through cooking recipe steps/ changed bowl illustration

--- src/views/CookRecipe.js
+++ src/views/CookRecipe.js
@@ -22,7 +22,7 @@ const CookRecipe = () => {
     const [progress, setProgress] = useState('0%');
     const [currentStep, setCurrentStep] = useState(null);
     const [nextStep, setNextStep] = useState(null);
-    // const nextStep = currentStep ? currentStep +1 : 2;
+    
     const bowl = bowlIllustration
     const db = getFirestore()
 
@@ -34,8 +34,6 @@ const CookRecipe = () => {
             setRecipe(docSnap.data())
             getSteps(docSnap.data())
             getStepIndex(parseInt(params.stepId, 10))
-            getCurrentStep(stepIndex)
-            getNextStep(stepIndex)
             getStep(docSnap.data(), (parseInt(params.stepId, 10)-1))
             getProgress((parseInt(params.stepId, 10)),docSnap.data().instructions.length)
             setLoading(false)
@@ -57,23 +55,25 @@ const CookRecipe = () => {
         setStep(recipe.instructions[stepIndex])
     }
 
-    function getCurrentStep(stepIndex) {
-        setCurrentStep(stepIndex+1)
-    }
-
-    function getNextStep(stepIndex) {
-        setNextStep(stepIndex+2)
-    }
-
     function getProgress(currentStep, steps) {
         const progress = (currentStep / steps) * 100 + '%';
         setProgress(progress)
     }
 
+    function updateState() {
+        setLoading(true)
+        setStepIndex((parseInt(params.stepId, 10)) -1)
+        setStep(recipe.instructions[stepIndex])
+        getProgress((parseInt(params.stepId, 10)),recipe.instructions.length)
+        setLoading(false)
+    }
+
     useEffect(() => {
+        setCurrentStep(parseInt(params.stepId, 10))
+        setNextStep(parseInt(params.stepId, 10)+1)
         getRecipeData()
         // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
+      }, [params.stepId]);
 
   return (
     <div className="relative text-gray-700 w-full min-h-screen h-max">
@@ -90,10 +90,10 @@ const CookRecipe = () => {
             <div className="w-full bg-brand-light-teal h-4 rounded-md">
                 <div className="bg-brand-teal h-4 z-20 rounded-md" style={{ width: `${progress}`}}  />
             </div>
-            <div className="pt-4">
+            <div className="pt-4 leading-relaxed text-gray-800">
                 {loading ? 'Loading' : step}
             </div>
-            <img className="absolute z-0 bottom-56 w-full flex-none inset-x-0 opacity-30" src={bowl} alt="bowl" />
+            <img className="absolute z-0 bottom-56 w-2/3 mx-auto flex-none inset-x-0 opacity-30" src={bowl} alt="bowl" />
         </div>
         { loading ? 
         (
@@ -101,7 +101,7 @@ const CookRecipe = () => {
                 <span>Loading</span>
             </div>
         ) : (
-            <Link to={currentStep < steps ? `/recipe/${recipeId}/step/${nextStep}` : `/`} className="fixed bottom-24 w-full py-6 bg-brand-orange text-white tracking-wide font-semibold text-xl flex flex-row justify-center items-center space-x-6">
+            <Link onClick={updateState} to={currentStep < steps ? `/recipe/${recipeId}/step/${nextStep}` : `/`} className="fixed bottom-24 w-full py-6 bg-brand-orange text-white tracking-wide font-semibold text-xl flex flex-row justify-center items-center space-x-6">
                 <span>{currentStep < steps ? 'CONTINUE' : 'FINISH'}</span>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M10.8366 12.3607L10.835 12.0066C10.835 10.535 10.9212 9.19332 11.051 8.31871L11.1648 7.77483C11.228 7.48678 11.3112 7.1588 11.3979 6.99137C11.7154 6.37892 12.3361 6 13.0005 6H13.0583C13.4914 6.01432 14.4012 6.39435 14.4012 6.40756C15.8652 7.02183 18.6895 8.87572 19.994 10.1974L20.3731 10.5942C20.4723 10.7017 20.5839 10.829 20.6531 10.9282C20.8844 11.2344 21 11.6134 21 11.9923C21 12.4153 20.8702 12.8085 20.6247 13.1302L20.2353 13.5505L20.148 13.6402C18.9644 14.9234 15.8739 17.0218 14.2572 17.664L14.0131 17.7576C13.7194 17.8629 13.3078 17.9884 13.0583 18C12.7408 18 12.4376 17.9262 12.1485 17.7808C11.7874 17.577 11.4994 17.2554 11.3401 16.8764C11.2387 16.6143 11.0794 15.8267 11.0794 15.8124C10.9334 15.0183 10.8487 13.7652 10.8366 12.3607ZM3 11.9996C3 11.1613 3.67308 10.4817 4.50325 10.4817L8.20248 10.8088C8.85375 10.8088 9.38174 11.3419 9.38174 11.9996C9.38174 12.6583 8.85375 13.1903 8.20248 13.1903L4.50325 13.5175C3.67308 13.5175 3 12.8378 3 11.9996Z" fill="white"/>
