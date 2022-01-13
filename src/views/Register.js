4699ff1143eae985f050642b8eb8601901cf74be
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2022-01-13 21:52:49 +0000

    Fixed various stuff

--- src/views/Register.js
+++ src/views/Register.js
@@ -16,20 +16,21 @@ const Register = (props) => {
   const auth = getAuth();
 
   const handleEmailRegister = async (data) => {
-    const { email, password } = data;
+    try {
+      const { email, password } = data;
       await createUserWithEmailAndPassword(auth, email, password)
       await navigate('../', { replace: true })
-      .catch((error) => {
-        setServerErrorMessage(error.message)
-      });
+    } catch (error) {
+      setServerErrorMessage(error.message)
     }
-
+  }
 
   const handleSocialSubmit = async (method) => {
     try {
       if (method === "google") {
         await signInGoogleUser();
       }
+      await navigate('../', { replace: true })
     } catch (error) {
       console.log("error");
     }
@@ -40,7 +41,7 @@ const Register = (props) => {
         <div className="flex flex-col justify-center align-center text-center">
         <img src={logo} alt="logo" className="w-1/3 max-w-min self-center flex flex-shrink-0 mb-4"/>
           <h1 className="text-4xl font-galada mt-4 pb-2">Student Cookbook</h1>
-          <p className="text-sm px-10">A vast collection of fast and easy recipes with a step by step guide</p>
+          <p className="text-sm px-10">A vast collection of fast and easy recipes with a step by step guide.</p>
         </div>
         <Form
           serverErrorMessage={serverErrorMessage}
